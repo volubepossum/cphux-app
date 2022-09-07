c14fdd282577e49566173330d698be0c93bcfb46
volubepossum <domj.roli@gmail.com>
2022-09-08 00:30:41 +0200

    init laravel app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Hello CPHUX</title>
+  <meta name="description" content="A simple project for interview.">
+  <meta name="author" content="Roland">
+
+  <meta property="og:title" content="Hello CPHUX">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="A simple project for interview.">
+  <meta property="og:image" content="image.png">
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  @livewireStyles
+
+
+</head>
+
+<body>
+  <!-- your content here... -->
+  @livewireScripts
+</body>
+</html>
